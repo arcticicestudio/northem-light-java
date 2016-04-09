55c7e2aa6434deb309d50433dbd32c4df1435db9
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 10:51:20 +0200

    Implement all color constant variables

--- src/main/java/com/arcticicestudio/northem/light/NorthemLight.java
+++ src/main/java/com/arcticicestudio/northem/light/NorthemLight.java
@@ -32,4 +32,80 @@ Arctic Versioning Specification
 */
 package com.arcticicestudio.northem.light;
 
-public enum NorthemLight {}
+public enum NorthemLight {
+  /**
+   * The color {@code dark-gray} with an RGB value of {@code rgb(69,69,70)} and HEX value of {@code #454546}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(69,69,70);margin: 0"></div>
+   */
+  DARK_GRAY(69,69,70),
+  /**
+   * The color {@code gray} with an RGB value of {@code rgb(109,109,111)} and HEX value of {@code #6D6D6F}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(109,109,111);margin: 0"></div>
+   */
+  GRAY(109,109,111),
+  /**
+   * The color {@code light-gray} with an RGB value of {@code rgb(198,198,200)} and HEX value of {@code #C6C6C8}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(198,198,200);margin: 0"></div>
+   */
+  LIGHT_GRAY(198,198,200),
+  /**
+   * The color {@code dark-snow} with an RGB value of {@code rgb(242,242,247)} and HEX value of {@code #F2F2F7}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(242,242,247);margin: 0"></div>
+   */
+  DARK_SNOW(242,242,247),
+  /**
+   * The color {@code dirty-snow} with an RGB value of {@code rgb(249,249,252)} and HEX value of {@code #F9F9FC}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(249,249,252);margin: 0"></div>
+   */
+  DIRTY_SNOW(249,249,252),
+  /**
+   * The color {@code snow} with an RGB value of {@code rgb(255,255,255)} and HEX value of {@code #FFFFFF}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(255,255,255);margin: 0"></div>
+   */
+  SNOW(255,255,255),
+  /**
+   * The color {@code aqua} with an RGB value of {@code rgb(122,202,194)} and HEX value of {@code #7ACAC2}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(122,202,194);margin: 0"></div>
+   */
+  AQUA(122,202,194),
+  /**
+   * The color {@code light-blue} with an RGB value of {@code rgb(111,186,208)} and HEX value of {@code #6FBAD0}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(111,186,208);margin: 0"></div>
+   */
+  LIGHT_BLUE(111,186,208),
+  /**
+   * The color {@code blue} with an RGB value of {@code rgb(113,167,202)} and HEX value of {@code #71A7CA}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(113,167,202);margin: 0"></div>
+   */
+  BLUE(113,167,202),
+  /**
+   * The color {@code dark-blue} with an RGB value of {@code rgb(87,129,179)} and HEX value of {@code #5781B3}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(87,129,179);margin: 0"></div>
+   */
+  DARK_BLUE(87,129,179),
+  /**
+   * The color {@code red} with an RGB value of {@code rgb(223,92,90)} and HEX value of {@code #DF5C5A}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(223,92,90);margin: 0"></div>
+   */
+  RED(223,92,90),
+  /**
+   * The color {@code yellow} with an RGB value of {@code rgb(255,197,104)} and HEX value of {@code #FFC568}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(255,197,104);margin: 0"></div>
+   */
+  YELLOW(255,197,104),
+  /**
+   * The color {@code orange} with an RGB value of {@code rgb(241,144,84)} and HEX value of {@code #F19054}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(241,144,84);margin: 0"></div>
+   */
+  ORANGE(241,144,84),
+  /**
+   * The color {@code lime} with an RGB value of {@code rgb(197,201,97)} and HEX value of {@code #C5C961}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(197,201,97);margin: 0"></div>
+   */
+  LIME(197,201,97),
+  /**
+   * The color {@code purple} with an RGB value of {@code rgb(186,142,196)} and HEX value of {@code #BA8EC4}.
+   * <div style="border:none;width:25px;height:30px;background-color:rgb(186,142,196);margin: 0"></div>
+   */
+  PURPLE(186,142,196);
+}
