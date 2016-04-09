e320255f5bb2737ecb65a3aa06e59256029d4e3c
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 10:56:26 +0200

    Create JUnit test class "NorthemLightTest"

---
+++ src/test/java/com/arcticicestudio/northem/light/NorthemLightTest.java
@@ -0,0 +1,49 @@
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++
+title     Northem Light Enumeration JUnit Test +
+project   northem-light-java                   +
+file      NorthemLightTest.java                +
+version                                        +
+author    Arctic Ice Studio                    +
+email     development@arcticicestudio.com      +
+website   http://arcticicestudio.com           +
+copyright Copyright (C) 2016                   +
+created   2016-01-24 19:59 UTC+0100            +
+modified  2016-04-09 10:27 UTC+0200            +
+++++++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+JUnit test class for the 'NorthemLight' class of the "Northem Light - Java" API.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+Northem Light
+  (https://github.com/arcticicestudio/northem-light)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JavaFX 2.2 API Documentation
+  (https://docs.oracle.com/javafx/2/api/)
+JavaFX 2.2 Oracle Project Website
+  (http://docs.oracle.com/javase/8/javase-clienttechnologies.htm)
+Arctic Versioning Specification
+  (http://specs.arcticicestudio.com/arcver) (ArcVer)
+*/
+
+package com.arcticicestudio.northem.light;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * JUnit test class for the {@link NorthemLight} class of the
+ * <a href="https://github.com/arcticicestudio/northem-light-java">Northem Light - Java</a> API.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @see NorthemLight
+ * @see <a href="http://junit.org/">http://junit.org/</a>
+ * @see <a href="https://github.com/arcticicestudio/northem-light-java">Northem Light - Java</a>
+ * @since 0.1.0
+ */
+public class NorthemLightTest {}
