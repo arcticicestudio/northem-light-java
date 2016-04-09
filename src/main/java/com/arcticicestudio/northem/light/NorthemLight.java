b0a2ac23fd821f742cef09c1582f5bcdaae8c6fe
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 10:51:07 +0200

    Create enumeration class "NorthemLight"

---
+++ src/main/java/com/arcticicestudio/northem/light/NorthemLight.java
@@ -0,0 +1,35 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     NorthemLight                    +
+project   northem-light-java              +
+file      NorthemLight.java               +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+website   http://arcticicestudio.com      +
+copyright Copyright (C) 2016              +
+created   2016-01-24 19:46 UTC+0100       +
+modified  2016-04-09 10:23 UTC+0200       +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+"Northem Light" color palette as enumeration for the "Northem Light - Java" project.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+Northem Light
+  (https://github.com/arcticicestudio/northem-light)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JavaFX 2.2 API Documentation
+  (https://docs.oracle.com/javafx/2/api/)
+JavaFX 2.2 Oracle Project Website
+  (http://docs.oracle.com/javase/8/javase-clienttechnologies.htm)
+Arctic Versioning Specification
+  (http://specs.arcticicestudio.com/arcver) (ArcVer)
+*/
+package com.arcticicestudio.northem.light;
+
+public enum NorthemLight {}
